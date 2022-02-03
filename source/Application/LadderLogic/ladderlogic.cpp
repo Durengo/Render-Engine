54a52c9e752499c9207bc3adfbbafac56f5730f6
seb <sebastian.termen@gmail.com>
2022-02-03 20:19:53 +0200

    tiny refactor

---
+++ source/Application/LadderLogic/ladderlogic.cpp
@@ -0,0 +1,18 @@
+#include "gates/ladderLogicHeader.h"
+#include "utils/LocalHeaderPreLoader.h"
+
+#include "tests/testUnit.h"
+
+int main()	{
+		testUnit test;
+		test.setGlobalTick(1);
+
+
+//		ladderLogic::andGate gate2("and gate");
+//		std::cout << gate2.getOutput();
+//
+//		ladderLogic::signalNode signalinput;
+//		signalinput.print();
+
+		return 0;
+}
